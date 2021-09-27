159546077ecaf8dc21389b4f9cd0b5a3502bf0cf
is_gordeev <gordeev.john@gmail.com>
2021-09-28 00:13:53 +0300

    Update script

--- html-gen.js
+++ html-gen.js
@@ -1,46 +1,3 @@
-// const regexps = {
-//     $ignore: /^[ \t\n]$/,
-//     string: /(\"([^\\\"]|\\.)*\")/,
-//     keyword: [
-//         /(\b(pub|const|type|enum|struct|trait|static|func|party)\b)/,
-//         /(\b(else)\b)/,
-//         /\b(break|continue|elif|if|in|for|loop|match|return|while)\b/,
-//         /(_)/,
-//     ],
-//     modifier: [
-//         /\b(mut)\b/,
-//         /\b(ref)\b/,
-//     ],
-//     terminator: /(;)/,
-//     separator: /(,)/,
-//     punctuation: /(::|\{|\}|\(|\)|\[|\])/,
-//     operator: [
-//         /(\+=|-=|\/=|\*=|%=|\^=|&=|\\|=|<<=|>>=|=)/,
-//         /(<=|>=|<|>|==|!=)/,
-//         /(!|\+(?!\+)|-(?!-)|\/|\*|%|\^|\&|\||<<|>>)/,
-//         /\b(and|or)\b/,
-//         /:(?!:)/,
-//     ],
-//     constant: [
-//         /\b(true|false)\b/,
-//         /(([0-9][0-9_]*)(?:([ui](?:8|16|32|64|128)|i|u))?)/,
-//         /((0x[a-fA-F0-9_]+)(?:([ui](?:8|16|32|64|128)|i|u))?)/,
-//         /((0o[0-7_]+)(?:([ui](?:8|16|32|64|128)|i|u))?)/,
-//         /((0b[01_]+)(?:([ui](?:8|16|32|64|128)|i|u))?)/,
-//         /(([0-9][0-9_]*\.[0-9][0-9_]*))/,
-//         /(([0-9][0-9_]*(?:\.[0-9][0-9_]*)?)(f32|f64|f|d))/,
-//         /(([0-9][0-9_]*(?:\.[0-9][0-9_]*)?[eE][+-]?[0-9_]+)(f32|f64|f|d)?)/,
-//     ],
-//     type: [
-//         /([A-Z][a-zA-Z0-9_]+)/,
-//         /\b(bool|char|[ui](?:8|16|32|64|128)|f(?:16|32|64|128)|int|uint|str|String|Self|Option|Result)\b/,
-//     ],
-//     func: /([\w_]+)(?=\()/,
-//     variable: /(\w+)/,
-//     other: /(\W)/,
-// }
-
-
 const fs = require('fs')
 const path = require('path')
 
@@ -55,80 +12,78 @@ const regexps = [
 
     /(?<string>(\"([^\\\"]|\\.)*\"))/,
 
-    /(?<keyword>(\b(pub|const|type|enum|struct|trait|static|func|party)\b))/,
-    /(?<keyword>(\b(else)\b))/,
-    /(?<keyword>\b(break|continue|elif|if|in|for|loop|match|return|while)\b)/,
-    /(?<keyword>(_))/,
+    /(?<keyword_other>(\b(pub|const|type|enum|struct|trait|static|func|party|else)\b))/,
+    // /(?<keyword_else>(\b(else)\b))/,
+    /(?<keyword_control>\b(break|continue|elif|if|in|for|loop|match|return|while)\b)/,
+    /(?<keyword_underscore>(_))/,
 
-    /(?<modifier>\b(mut)\b)/,
-    /(?<modifier>\b(ref)\b)/,
+    /(?<modifier_mut>\b(mut)\b)/,
+    /(?<modifier_ref>\b(ref)\b)/,
 
     /(?<terminator>(;))/,
     /(?<separator>(,))/,    
     /(?<punctuation>(::|\{|\}|\(|\)|\[|\]))/,
 
-    /(?<operator>(\+=|-=|\/=|\*=|%=|\^=|&=|\\|=|<<=|>>=|=))/,
-    /(?<operator>(<=|>=|<|>|==|!=))/,
-    /(?<operator>(!|\+(?!\+)|-(?!-)|\/|\*|%|\^|\&|\||<<|>>))/,
-    /(?<operator>\b(and|or)\b)/,
-    /(?<operator>:(?!:))/,
+    /(?<operator_assign>(\+=|-=|\/=|\*=|%=|\^=|&=|\\|=|<<=|>>=|=))/,
+    /(?<operator_cmp>(<=|>=|<|>|==|!=))/,
+    /(?<operator_math>(!|\+(?!\+)|-(?!-)|\/|\*|%|\^|\&|\||<<|>>))/,
+    /(?<operator_logic>\b(and|or)\b)/,
+    /(?<operator_punctuation>:(?!:))/,
 
-    /(?<constant>\b(true|false)\b)/,
-    /(?<constant>(([0-9][0-9_]*)(?:([ui](?:8|16|32|64|128)|i|u))?))/,
-    /(?<constant>((0x[a-fA-F0-9_]+)(?:([ui](?:8|16|32|64|128)|i|u))?))/,
-    /(?<constant>((0o[0-7_]+)(?:([ui](?:8|16|32|64|128)|i|u))?))/,
-    /(?<constant>((0b[01_]+)(?:([ui](?:8|16|32|64|128)|i|u))?))/,
-    /(?<constant>(([0-9][0-9_]*\.[0-9][0-9_]*)))/,
-    /(?<constant>(([0-9][0-9_]*(?:\.[0-9][0-9_]*)?)(f32|f64|f|d)))/,
-    /(?<constant>(([0-9][0-9_]*(?:\.[0-9][0-9_]*)?[eE][+-]?[0-9_]+)(f32|f64|f|d)?))/,
+    /(?<constant_bool>\b(true|false)\b)/,
+    /(?<constant_dec>(([0-9][0-9_]*)(?:([ui](?:8|16|32|64|128)|i|u))?))/,
+    /(?<constant_hex>((0x[a-fA-F0-9_]+)(?:([ui](?:8|16|32|64|128)|i|u))?))/,
+    /(?<constant_oct>((0o[0-7_]+)(?:([ui](?:8|16|32|64|128)|i|u))?))/,
+    /(?<constant_bin>((0b[01_]+)(?:([ui](?:8|16|32|64|128)|i|u))?))/,
+    /(?<constant_float>(([0-9][0-9_]*\.[0-9][0-9_]*)))/,
+    /(?<constant_float_suffix>(([0-9][0-9_]*(?:\.[0-9][0-9_]*)?)(f32|f64|f|d)))/,
+    /(?<constant_float_exp>(([0-9][0-9_]*(?:\.[0-9][0-9_]*)?[eE][+-]?[0-9_]+)(f32|f64|f|d)?))/,
 
-    /(?<type>([A-Z][a-zA-Z0-9_]+))/,
-    /(?<type>\b(bool|char|[ui](?:8|16|32|64|128)|f(?:1…|128)|int|uint|str|String|Self|Option|Result)\b)/,
+    /(?<type_word>([A-Z][a-zA-Z0-9_]+))/,
+    /(?<type_builtin>\b(bool|char|[ui](?:8|16|32|64|128)|f(?:1…|128)|int|uint|str|String|Self|Option|Result)\b)/,
 
     /(?<func>([\w_]+)(?=\())/,
     /(?<variable>(\w+))/,
     /(?<other>(\W))/
 ]
 
-const fullRegex = new RegExp(Object.values(regexps).map(r => {
-    if (Array.isArray(r)) {
-        for (const subr of r) {
-            return subr.source
-        }
-    } else {
-        return r.source
-    }
-}).join('|'), 'g')
+const fullRegex = new RegExp(Object.values(regexps).map(r => r.source).join('|'), 'g')
 
 const tmpl = (rule, m) => {
     if (rule === '$ignore') {
         return m
     }
+    let classes = []
+    let prefix = []
+    for (const seg of rule.split('_')) {
+        classes.push(seg + prefix.join('.'))
+        prefix.push(seg)
+    }
     return `
-<span class="${rule}">${m}</span>
+<span class="${classes.join(' ')}">${m}</span>
     `.trim()
 }
 
 const process = (src, theme) => {
     const hl = src.replace(fullRegex, (m, ...args) => {
-        // console.log(m, ...args);
         // m = args.slice(0, args.length - 2).filter(el => !!el)[0]
         // if (!m) {
         // throw new Error('wtf')
         // }
-        for (const [rule, regex] of Object.entries(regexps)) {
+        for (const [regex] of Object.entries(regexps)) {
             // !Array.isArray(regex) && console.log(`check '${m}' for`, rule, regex)
-
-            if (Array.isArray(regex)) {
-                for (const r of regex) {
-                    // console.log(`check '${m}' for`, rule, r)
-                    if (r.test(m)) {
-                        return tmpl(rule, m)
-                    }
-                }
-            } else if (regex.test(m)) {
-                return tmpl(rule, m)
+            const match = m.match(regex)
+            if (!match) {
+                continue
             }
+
+            const rule = match.groups[Object.keys(match.groups)[0]]
+
+            if (!rule) {
+                throw new Error('Wtf 2.0')
+            }
+
+            return tmpl(rule, match[rule])
         }
 
         throw new Error(`WTF???!?!?!: '${m}'`)
